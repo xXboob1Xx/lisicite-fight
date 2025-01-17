b3af80f19f6708c0f2322e18b2c3d4ebb65439fc
xXboob1Xx <aleksadar2007@gmail.com>
2025-01-17 20:41:09 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
+    //alo
     }
 }
